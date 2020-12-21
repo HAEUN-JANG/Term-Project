e380fc897ab25755edac12537bb789888fe758ac
HAEUN-JANG <ha1792@naver.com>
2020-12-22 02:14:01 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+const nunjucks = require('nunjucks');
+const createError = require('http-errors');
+const pageRouter = require('./routes/page.router');
+
+const app = express();  // express Application 객체 생성
+nunjucks.configure('views', {
+    express: app,
+    watch: true
+});
+
+app.set('view engine', 'html');
+// 커스텀 미들웨어 등록
+app.use(morgan('dev'));
+app.use('/public', express.static(path.join(__dirname, 'public')));
+app.use('/', pageRouter);
+
+app.use((req, res, next) => {
+    next(createError(404));
+});
+
+app.use((err, req, res, next) => {
+    res.locals.message = err.message;
+    res.locals.status = err.status;
+    res.render('error');
+});
+// 루트 경로(/)로 GET 요청이 왔을 경우 응답
+
+app.listen(3000, () => console.log('Server running on 3000!'));
---
+++ routes/page.router.js
@@ -0,0 +1,13 @@
+const { Routes, Router } = require('express');
+
+const router = Router();
+
+router.get('/', (req, res) => {
+    //db에서 user 정보를 가져오는 코드 작성(db쓸경우)
+    res.render('index', { title: 'TermProject', username: '장하은' });
+});
+//내가 만약 회원가입 페이지가 필요하다!
+router.get('/write',(req,res)=>{
+    res.render('write');
+});
+module.exports = router;
